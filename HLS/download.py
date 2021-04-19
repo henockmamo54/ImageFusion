a98aac02f9423849d2bac9f40d459eed53c91aa3
DESKTOP-9KF3V27\Henock <henockmamo54@gmail.com>
2021-04-19 16:48:00 +0900

    landsat8 download script added

---
+++ HLS/download.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 13 12:04:56 2021
+
+@author: Henock
+"""
+
+import os 
+
+path="~/../../bess19/Image_fusion/download/landsat8/Collection_2/L2/"
+
+os.system('cd {}'.format(path)) 
+os.system('find . -size 0 -delete')  
+os.system('nohup python {0}downloadscript.py > {0}log_C2L2.txt &'.format(path))
+
